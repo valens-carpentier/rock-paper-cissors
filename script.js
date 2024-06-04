72f566b9ac4f277d4bbfdea78127736bc77527db
zalenscarpentier <168520191+zalenscarpentier@users.noreply.github.com>
2024-06-04 15:24:03 +0200

    build playGame function

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ function getComputerChoice () {
     } else if (randomNumber === 2) {
         return computerChoice = "Paper"; // If the number is 2, the computer choice is paper
     }
-
 }
 
 function getHumanChoice () {
     let humanChoice;
-    let userInput = prompt("Rock? Paper? Scissor"); // Define the variable asking to the user
-    if (userInput === "Rock") {
+    let userInput = prompt("Rock? Paper? Scissor").toLowerCase(); // Define the variable asking to the user
+    if (userInput === "rock") {
         return humanChoice = "Rock"
-    } else if (userInput === "Scissor") {
+    } else if (userInput === "scissor") {
         return humanChoice = "Scissor"
-    } else if (userInput === "Paper") {
-        return humanChoice === "Paper"
+    } else if (userInput === "paper") {
+        return humanChoice = "Paper"
     } else {
+        console.log("Invalid input, please try again."); // Log invalid input
         return getHumanChoice();
     }
 }
@@ -30,38 +30,53 @@ let humanScore = 0;
 let computerScore = 0;
 
 // Write the logic to determine a winner on one hand
-function playRound (getHumanChoice,getHumanChoice) {
-    if (getComputerChoice === "Rock" & getHumanChoice === "Rock") { // Both players choose Rock: (R, R), no point added
-        return "Tie! No ones wins";
-    } else if (getHumanChoice === "Rock" & getComputerChoice === "Paper") { // User chooses Rock, Computer chooses Paper: (R, P) - Computer wins
-        computerScore = computerScore + 1;
-        return "You lose! Paper beats Rock";
-    } else if (getHumanChoice === "Rock" & getComputerChoice === "Scissor") {  // User chooses Rock, Computer chooses Scissors: (R, S) - Users wins
-        humanScore = humanScore + 1;
-        return "You win! Rock beats Scissor";
-    } else if (getHumanChoice === "Paper" & getComputerChoice === "Rock") { // User chooses Paper, Computer chooses Rock: (P, R) - User wins
-        humanScore = humanScore + 1;
-        return "You win! Paper beats Rock";
-    } else if (getHumanChoice === "Paper" & getComputerChoice === "Paper") { // Both players choose Paper: (P, P) - , no point added
-        return "Tie! No ones wins";
-
+function playRound (humanChoice,computerChoice) {
+    if (computerChoice === "Rock" && humanChoice === "Rock") {
+        console.log("Tie! No one wins");
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        computerScore++;
+        console.log("You lose! Paper beats Rock");
+    } else if (humanChoice === "Rock" && computerChoice === "Scissor") {
+        humanScore++;
+        console.log("You win! Rock beats Scissor");
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        humanScore++;
+        console.log("You win! Paper beats Rock");
+    } else if (humanChoice === "Paper" && computerChoice === "Paper") {
+        console.log("Tie! No one wins");
+    } else if (humanChoice === "Paper" && computerChoice === "Scissor") {
+        computerScore++;
+        console.log("You lose! Scissor beats Paper");
+    } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
+        computerScore++;
+        console.log("You lose! Rock beats Scissor");
+    } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
+        humanScore++;
+        console.log("You win! Scissor beats Paper");
+    } else if (humanChoice === "Scissor" && computerChoice === "Scissor") {
+        console.log("Tie! No one wins");
     }
 }
 
+function playGame () {
 
-    
-    // User chooses Paper, Computer chooses Scissors: (P, S) - Computers wins
-    // User chooses Scissors, Computer chooses Rock: (S, R) - Computers wins
-    // User chooses Scissors, Computer chooses Paper: (S, P) - User wins
-    // Both players choose Scissors: (S, S) - , no point added
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound (humanChoice,computerChoice);
+        console.log("Human:",humanScore,"Computer:",computerScore)
 
-// Write the logic to determine who is the winner after 5 round
-    // The party must be five rounds
-    // Each time, add the point wins to the winners to the variable scores
-    // When done 5 rounds, display the result
-    // If computer is above user, computer wins
-    // If computer is behind user, cuser wons
-    // If computer is equal to user, tie game
+    }
+    if (humanScore > computerScore) {
+        console.log("You won the game!")
+    } else if (humanScore < computerScore) {
+        console.log("You lose the game!")
+    } else if (humanScore === computerScore) {
+        console.log("Tie Game")
+    }
+}
+
+playGame();
 
 
 
